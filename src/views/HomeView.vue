adb7f6516f98a68ebed6ed024aaa3c4e808bb9b5
kaede96az <kaede96az@gmail.com>
2023-10-16 17:56:43 +0900

    chore: format

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -36,58 +36,65 @@ const selectTileColor = (title: string): string => {
       return 'orange-accent-1'
   }
 }
-
 </script>
 
 <template>
   <v-container>
-  <h4 class="text-h4">報告数</h4>
-  <v-row>
-  <v-col cols="12" sm="4" v-for="item in items.reported_issues" :key="item.title">
-    <SummaryCard
-      :icon="selectIcon(item.title)"
-      :tile_color="selectTileColor(item.title)"
-      :title="item.title"
-      :data="item.data"
-      :unit="item.unit"
-    />
-  </v-col>
-  </v-row>
+    <h4 class="text-h4">報告数</h4>
+    <v-row>
+      <v-col cols="12" sm="4" v-for="item in items.reported_issues" :key="item.title">
+        <SummaryCard
+          :icon="selectIcon(item.title)"
+          :tile_color="selectTileColor(item.title)"
+          :title="item.title"
+          :data="item.data"
+          :unit="item.unit"
+        />
+      </v-col>
+    </v-row>
   </v-container>
 
   <v-container>
-  <h4 class="text-h4">因果関係を認定済み</h4>
-  <v-row>
-  <v-col cols="12" sm="6" v-for="item in items.certified_issues" :key="item.title">
-    <SummaryCard
-      :icon="selectIcon(item.title)"
-      :tile_color="selectTileColor(item.title)"
-      :title="item.title"
-      :data="item.data"
-      :unit="item.unit"
-    />
-  </v-col>
-  </v-row>
+    <h4 class="text-h4">因果関係を認定済み</h4>
+    <v-row>
+      <v-col cols="12" sm="6" v-for="item in items.certified_issues" :key="item.title">
+        <SummaryCard
+          :icon="selectIcon(item.title)"
+          :tile_color="selectTileColor(item.title)"
+          :title="item.title"
+          :data="item.data"
+          :unit="item.unit"
+        />
+      </v-col>
+    </v-row>
   </v-container>
 
   <v-container>
-  <v-row>
-  <v-col cols="12" sm="6">
-    <apexchart
-      height="400"
-      type="bar"
-      :options="{
-        chart: {id: 'number_of_deaths_reported_by_age_group'},
-        colors: ['#c83f3d'],
-        title: {text: '年代別死亡報告数', floating: true},
-        xaxis: {title: {text: '年代'}, categories: items.number_of_deaths_reported_by_age_group.x_axis.data},
-        yaxis: {title: {text: '死亡報告数 (人)'}},
-        plotOptions: { bar: {horizontal: true, borderRadius: 2}}
-      }"
-      :series="[{name: items.number_of_deaths_reported_by_age_group.y_axis.name, data: items.number_of_deaths_reported_by_age_group.y_axis.data}]"
-    ></apexchart>
-  </v-col>
-  </v-row>
+    <v-row>
+      <v-col cols="12" sm="6">
+        <apexchart
+          height="400"
+          type="bar"
+          :options="{
+            chart: { id: 'number_of_deaths_reported_by_age_group' },
+            colors: ['#c83f3d'],
+            title: { text: '年代別死亡報告数', floating: true },
+            xaxis: {
+              title: { text: '年代' },
+              categories: items.number_of_deaths_reported_by_age_group.x_axis.data
+            },
+            yaxis: { title: { text: '死亡報告数 (人)' } },
+            plotOptions: { bar: { horizontal: true, borderRadius: 2 } }
+          }"
+          :series="[
+            {
+              name: items.number_of_deaths_reported_by_age_group.y_axis.name,
+              data: items.number_of_deaths_reported_by_age_group.y_axis.data
+            }
+          ]"
+        ></apexchart>
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
