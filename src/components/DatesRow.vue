842499d6d122f2441c265733387721e22b151eb3
kaede96az <147464913+kaede96az@users.noreply.github.com>
2023-12-15 17:11:43 +0900

    fix: 複数行表示の判断ロジックで要素1つの場合がまちがっていたのを修正

--- src/components/DatesRow.vue
+++ src/components/DatesRow.vue
@@ -1,5 +1,8 @@
 <template>
-	<div v-if="dates.length > 1">
+	<div v-if="dates.length == 1">
+		<span>{{ dates[0] }}</span>
+    </div>
+	<div v-else-if="dates.length > 1">
 		<span>{{ dates[0] + '...' }}</span>
     </div>
 	<div v-else>-</div>
